ca2f7146f42fcb735a6fead3e096683ccd0f6f4e
Jarett Millard <jarett.millard@gmail.com>
2016-03-16 00:51:58 -0400

    Adds annotations for default primitive and String values

---
+++ src/main/java/com/ryanharter/auto/value/moshi/annotation/DefaultFloat.java
@@ -0,0 +1,18 @@
+package com.ryanharter.auto.value.moshi.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Sets the default value for a float field if not present in deserialized JSON.
+ */
+@Target({METHOD})
+@Retention(RUNTIME)
+@Documented
+public @interface DefaultFloat {
+  float value();
+}
