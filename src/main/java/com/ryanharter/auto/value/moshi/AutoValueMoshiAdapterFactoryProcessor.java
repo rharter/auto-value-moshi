d2f01103b0af61bff259062ef4f4a970372f34ae
Ryan Harter <ryanjharter@gmail.com>
2016-04-27 10:40:01 -0500

    Adds a generator to create a JsonAdapter.Factory which includes all applicable `@AutoValue` annotated class JsonAdapters. (#30)

---
+++ src/main/java/com/ryanharter/auto/value/moshi/AutoValueMoshiAdapterFactoryProcessor.java
@@ -0,0 +1,161 @@
+package com.ryanharter.auto.value.moshi;
+
+import com.google.auto.service.AutoService;
+import com.google.auto.value.AutoValue;
+import com.google.auto.value.extension.AutoValueExtension;
+import com.google.common.collect.ImmutableSet;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.WildcardTypeName;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.ElementFilter;
+import javax.tools.Diagnostic;
+
+import static javax.lang.model.element.Modifier.FINAL;
+import static javax.lang.model.element.Modifier.PUBLIC;
+
+/**
+ * Created by rharter on 4/27/16.
+ */
+@AutoService(Processor.class)
+public class AutoValueMoshiAdapterFactoryProcessor extends AbstractProcessor {
+
+  private final AutoValueMoshiExtension extension = new AutoValueMoshiExtension();
+
+  @Override
+  public Set<String> getSupportedAnnotationTypes() {
+    return ImmutableSet.of(AutoValue.class.getName());
+  }
+
+  @Override
+  public SourceVersion getSupportedSourceVersion() {
+    return SourceVersion.latestSupported();
+  }
+
+  @Override
+  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+    List<Element> elements = new LinkedList<Element>();
+    for (Element element : roundEnv.getElementsAnnotatedWith(AutoValue.class)) {
+      AutoValueExtension.Context context = new LimitedContext(processingEnv, (TypeElement) element);
+      if (extension.applicable(context)) {
+        elements.add(element);
+      }
+    }
+
+    if (!elements.isEmpty()) {
+      TypeSpec jsonAdapterFactory = createJsonAdapterFactory(elements);
+      JavaFile file = JavaFile.builder("com.ryanharter.auto.value.moshi", jsonAdapterFactory).build();
+      try {
+        file.writeTo(processingEnv.getFiler());
+      } catch (IOException e) {
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Failed to write TypeAdapterFactory: " + e.getLocalizedMessage());
+      }
+    }
+
+    return false;
+  }
+
+  private TypeSpec createJsonAdapterFactory(List<Element> elements) {
+    TypeSpec.Builder factory = TypeSpec.classBuilder(ClassName.get("com.ryanharter.auto.value.moshi", "AutoValueMoshiAdapterFactory"));
+    factory.addModifiers(PUBLIC, FINAL);
+    factory.addSuperinterface(TypeName.get(JsonAdapter.Factory.class));
+
+    ParameterSpec type = ParameterSpec.builder(Type.class, "type").build();
+    WildcardTypeName extendsAnnotation = WildcardTypeName.subtypeOf(Annotation.class);
+    ParameterSpec annotations = ParameterSpec
+        .builder(ParameterizedTypeName.get(ClassName.get(Set.class), extendsAnnotation), "annotations")
+        .build();
+    ParameterSpec moshi = ParameterSpec.builder(Moshi.class, "moshi").build();
+    ParameterizedTypeName result = ParameterizedTypeName.get(ClassName.get(JsonAdapter.class),
+        WildcardTypeName.subtypeOf(TypeName.OBJECT));
+    MethodSpec.Builder create = MethodSpec.methodBuilder("create")
+        .addModifiers(PUBLIC)
+        .addAnnotation(Override.class)
+        .addParameters(ImmutableSet.of(type, annotations, moshi))
+        .returns(result);
+
+    for (int i = 0; i < elements.size(); i++) {
+      Element element = elements.get(i);
+      if (i == 0) {
+        create.beginControlFlow("if ($N.equals($T.class))", type, element);
+      } else {
+        create.nextControlFlow("else if ($N.equals($T.class))", type, element);
+      }
+      ExecutableElement jsonAdapterMethod = getJsonAdapterMethod(element);
+      create.addStatement("return $T." + jsonAdapterMethod.getSimpleName() + "($N)", element, moshi);
+    }
+    create.nextControlFlow("else");
+    create.addStatement("return null");
+    create.endControlFlow();
+
+    factory.addMethod(create.build());
+    return factory.build();
+  }
+
+  private ExecutableElement getJsonAdapterMethod(Element element) {
+    ParameterizedTypeName jsonAdapterType = ParameterizedTypeName.get(
+        ClassName.get(JsonAdapter.class), TypeName.get(element.asType()));
+    for (ExecutableElement method : ElementFilter.methodsIn(element.getEnclosedElements())) {
+      if (method.getModifiers().contains(Modifier.STATIC)
+          && method.getModifiers().contains(Modifier.PUBLIC)) {
+        TypeMirror rType = method.getReturnType();
+        TypeName returnType = TypeName.get(rType);
+        if (returnType.equals(jsonAdapterType)) {
+          return method;
+        }
+      }
+    }
+    return null;
+  }
+
+  private static class LimitedContext implements AutoValueExtension.Context {
+
+    private final ProcessingEnvironment processingEnvironment;
+    private final TypeElement autoValueClass;
+
+    private LimitedContext(ProcessingEnvironment processingEnvironment, TypeElement autoValueClass) {
+      this.processingEnvironment = processingEnvironment;
+      this.autoValueClass = autoValueClass;
+    }
+
+    @Override public ProcessingEnvironment processingEnvironment() {
+      return processingEnvironment;
+    }
+
+    @Override public String packageName() {
+      String fqn = autoValueClass.getQualifiedName().toString();
+      return fqn.substring(0, fqn.lastIndexOf('.'));
+    }
+
+    @Override public TypeElement autoValueClass() {
+      return autoValueClass;
+    }
+
+    @Override public Map<String, ExecutableElement> properties() {
+      return null;
+    }
+  }
+}
---
+++ src/test/java/com/ryanharter/auto/value/moshi/AutoValueMoshiAdapterFactoryProcessorTest.java
@@ -0,0 +1,74 @@
+package com.ryanharter.auto.value.moshi;
+
+import com.google.auto.value.processor.AutoValueProcessor;
+import com.google.common.collect.ImmutableSet;
+import com.google.testing.compile.JavaFileObjects;
+import java.util.Arrays;
+import javax.tools.JavaFileObject;
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertAbout;
+import static com.google.testing.compile.JavaSourceSubjectFactory.javaSource;
+import static com.google.testing.compile.JavaSourcesSubjectFactory.javaSources;
+import static org.junit.Assert.*;
+
+/**
+ * Created by rharter on 4/27/16.
+ */
+public class AutoValueMoshiAdapterFactoryProcessorTest {
+
+  @Test public void generatesJsonAdapterFactory() {
+    JavaFileObject source1 = JavaFileObjects.forSourceString("test.Foo", ""
+        + "package test;\n"
+        + "import com.google.auto.value.AutoValue;\n"
+        + "import com.squareup.moshi.JsonAdapter;\n"
+        + "import com.squareup.moshi.Moshi;\n"
+        + "@AutoValue public abstract class Foo {\n"
+        + "  public static JsonAdapter<Foo> jsonAdapter(Moshi moshi) {\n"
+        + "    return null;\n"
+        + "  }\n"
+        + "  public abstract String getName();\n"
+        + "  public abstract boolean isAwesome();\n"
+        + "}");
+    JavaFileObject source2 = JavaFileObjects.forSourceString("test.Bar", ""
+        + "package test;\n"
+        + "import com.google.auto.value.AutoValue;\n"
+        + "import com.squareup.moshi.JsonAdapter;\n"
+        + "import com.squareup.moshi.Moshi;\n"
+        + "@AutoValue public abstract class Bar {\n"
+        + "  public static JsonAdapter<Bar> jsonAdapter(Moshi moshi) {\n"
+        + "    return null;\n"
+        + "  }\n"
+        + "  public abstract String getName();\n"
+        + "}");
+    JavaFileObject expected = JavaFileObjects.forSourceString("com.ryanharter.auto.value.moshi.AutoValueMoshiJsonAdapterFactory", ""
+        + "package com.ryanharter.auto.value.moshi;\n"
+        + "\n"
+        + "import com.squareup.moshi.JsonAdapter;\n"
+        + "import com.squareup.moshi.Moshi;\n"
+        + "import java.lang.Override;\n"
+        + "import java.lang.annotation.Annotation;\n"
+        + "import java.lang.reflect.Type;\n"
+        + "import java.util.Set;\n"
+        + "import test.Bar;\n"
+        + "import test.Foo;\n"
+        + "\n"
+        + "public final class AutoValueMoshiAdapterFactory implements JsonAdapter.Factory {\n"
+        + "  @Override public JsonAdapter<?> create(Type type, Set<? extends Annotation> annotations, Moshi moshi) {\n"
+        + "    if (type.equals(Foo.class)) {\n"
+        + "      return Foo.jsonAdapter(moshi);\n"
+        + "    } else if (type.equals(Bar.class)) {\n"
+        + "      return Bar.jsonAdapter(moshi);\n"
+        + "    } else {\n"
+        + "      return null;\n"
+        + "    }\n"
+        + "  }\n"
+        + "}");
+    assertAbout(javaSources())
+        .that(ImmutableSet.of(source1, source2))
+        .processedWith(new AutoValueMoshiAdapterFactoryProcessor())
+        .compilesWithoutError()
+        .and()
+        .generatesSources(expected);
+  }
+}
