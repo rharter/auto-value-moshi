768e768b8d6acd6d6e8458c01d78332ae2b52920
Zac Sweers <zsweers@uber.com>
2018-11-17 02:45:05 -0800

    Mark factory processor as an aggregating processor

--- auto-value-moshi-annotations/src/main/java/com/ryanharter/auto/value/moshi/MoshiAdapterFactory.java
+++ auto-value-moshi-annotations/src/main/java/com/ryanharter/auto/value/moshi/MoshiAdapterFactory.java
@@ -1,11 +1,12 @@
 package com.ryanharter.auto.value.moshi;
 
 import com.squareup.moshi.JsonAdapter.Factory;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.SOURCE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
  * Annotation to indicate that a given class should generate a concrete implementation of a
@@ -21,7 +22,7 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
  *   }
  * </pre></code>
  */
-@Retention(SOURCE)
+@Retention(CLASS)
 @Target(TYPE)
 public @interface MoshiAdapterFactory {
   /**
--- auto-value-moshi/src/main/java/com/ryanharter/auto/value/moshi/AutoValueMoshiAdapterFactoryProcessor.java
+++ auto-value-moshi/src/main/java/com/ryanharter/auto/value/moshi/AutoValueMoshiAdapterFactoryProcessor.java
@@ -15,6 +15,7 @@ import com.squareup.javapoet.TypeSpec;
 import com.squareup.javapoet.WildcardTypeName;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
+
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.ParameterizedType;
@@ -23,6 +24,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
