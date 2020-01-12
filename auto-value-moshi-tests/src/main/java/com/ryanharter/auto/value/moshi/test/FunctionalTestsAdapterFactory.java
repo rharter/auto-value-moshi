1061e1137f20fe338b7c67221b7112456f18ebd2
Zac Sweers <zsweers@slack-corp.com>
2020-01-11 23:28:50 -0500

    Ensure sane factory names in generation (#147)
    Resolves #144

--- auto-value-moshi-tests/src/main/java/com/ryanharter/auto/value/moshi/test/FunctionalTestsAdapterFactory.java
+++ auto-value-moshi-tests/src/main/java/com/ryanharter/auto/value/moshi/test/FunctionalTestsAdapterFactory.java
@@ -8,4 +8,12 @@ public abstract class FunctionalTestsAdapterFactory implements JsonAdapter.Facto
   public static JsonAdapter.Factory create() {
     return new AutoValueMoshi_FunctionalTestsAdapterFactory();
   }
+
+  // Regression test for https://github.com/rharter/auto-value-moshi/issues/144
+  @MoshiAdapterFactory
+  public abstract static class NestedFactoryForNaming implements JsonAdapter.Factory {
+    public static JsonAdapter.Factory create() {
+      return new AutoValueMoshi_FunctionalTestsAdapterFactory_NestedFactoryForNaming();
+    }
+  }
 }
